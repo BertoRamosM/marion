6b4e608b3a7eca2177792132a02a4fb905865d37
BertoRamosM <albertramos902@gmail.com>
2025-01-04 18:02:53 +0100

    about translated

--- src/app/[local]/components/About.jsx
+++ src/app/[local]/components/About.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 const About = () => {
+  const t = useTranslations("About"); 
+
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center gap-8 px-6 pt-8 pb-16 text-pretty">
       {/* Text Content Card */}
       <div className="max-w-lg p-8 rounded-3xl shadow-lg text-white z-10">
-        <h1 className="text-3xl font-bold text-[#ffa45b] text-pretty">Une prof diplômée qui te ressemble !</h1>
+        <h1 className="text-3xl font-bold text-[#ffa45b] text-pretty">{t("text1")}</h1>
         <p className="pt-4 text-black">
-          <p className='text-[#007ea7] font-bold'>Hello, moi c&apos;est Marion et je suis professeure de français langue étrangère à Rennes.</p> <br></br> 
+          <p className='text-[#007ea7] font-bold'>{t("text2")}</p> <br></br> 
           
-         <p> Comme toi, je suis une expat&apos; ! J&apos;ai longtemps vécu à l&apos;étranger, en Angleterre, au Mexique et en Espagne. Je parle d&apos;ailleurs couramment anglais, espagnol et catalan. 
-
-Avant de devenir prof, j&apos;étais guide touristique et je suis passionnée par l&apos;histoire de la Bretagne et de la Normandie (dont je suis originaire). 
+         <p> {t("text3")}
 </p>
 <br></br>
-Comme j&apos;aime transmettre et être en contact avec les cultures du monde entier, j&apos;ai repris des études pour devenir professeure et j&apos;ai passé le diplôme du DAEFLE (diplôme d&apos;aptitude à l&apos;enseignement du français langue étrangère). 
+{t("text4")}
 <br></br>
-<p className='pt-4'>J&apos;ai commencé à donner des cours de français dans un centre de langues du sud de la France à des expats venus du 4 coins du monde. J&apos;ai adoré cette expérience qui m&apos;a donné envie d&apos;ouvrir ma propre école. 
+<p className='pt-4'>{t("text5")}
+</p>
+<p className='pt-4'>{t("text6")}
 </p>
         </p>
         <p className="pt-6 text-black">
-        Je suis revenue en Bretagne et je lance maintenant WestFrench Academy, un concept unique à Rennes. 
+        {t("text7")}
 
         </p>
       </div>
