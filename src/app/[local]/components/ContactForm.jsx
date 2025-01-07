fe12cd45c5a0e7c6ea16d9538289ae5c8697a63a
Alberto <a.ramos@cyim.com>
2025-01-07 16:22:30 +0100

    contact form

--- src/app/[local]/components/ContactForm.jsx
+++ src/app/[local]/components/ContactForm.jsx
@@ -22,6 +22,7 @@ const ContactForm = () => {
           name="contact"
           method="POST"
           data-netlify="true"
+          
         >
             <input type="hidden" name="form-name" value="contact" />
 
