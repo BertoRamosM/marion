d6861bd3085a3ac4aacb501e71da71ec13014f3b
Alberto <a.ramos@cyim.com>
2025-01-07 14:23:45 +0100

    working on contact form

--- src/app/[local]/components/ContactForm.jsx
+++ src/app/[local]/components/ContactForm.jsx
@@ -64,7 +64,6 @@ const ContactForm = () => {
           className="space-y-6"
           name="contact"
           method="POST"
-          action="/"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
           onSubmit="submit"
