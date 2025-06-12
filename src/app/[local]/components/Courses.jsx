79e500a5c2770f3645df98e829b4db716a01913b
Alberto <a.ramos@cyim.com>
2025-06-12 18:55:54 +0200

    shcdule

--- src/app/[local]/components/Courses.jsx
+++ src/app/[local]/components/Courses.jsx
@@ -59,22 +59,22 @@ const Courses = () => {
             <h2 className="text-lg font-semibold text-[#007ea7] flex items-center gap-2">
               <CalendarIcon /> {t('text16')}
             </h2>
-            <div className="mt-4 grid grid-cols-3 text-gray-700 text-center px-8 text-xs md:text-sm">
-              <div>
+            <div className="mt-4 grid grid-cols-1 sm:grid-cols-3 text-gray-700 text-center px-8 text-xs md:text-sm gap-4 sm:gap-1">
+              <div className='flex flex-col gap-1'>
                 <h3 className="text-[#007ea7] font-bold">{t('text17')}</h3>
-                <p className='border-b-2 border-r-2 border-[#007ea7]'><span className='font-bold'>{t('text188')}</span>{t('text18')}</p>
-                <p className='border-b-2 border-[#007ea7] border-r-2'><span className='font-bold'>{t('text199')}</span>{t('text19')}</p>
-                <p className='border-b-2 border-[#007ea7] border-r-2'><span className='font-bold'>{t('text200')}</span>{t('text20')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] p-1'><span className='font-bold'>{t('text188')}</span>{t('text18')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] p-1'><span className='font-bold'>{t('text199')}</span>{t('text19')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] p-1'><span className='font-bold'>{t('text200')}</span>{t('text20')}</p>
               </div>
-              <div>
+              <div className='flex flex-col gap-1'>
                 <h3 className="text-[#007ea7] font-bold">{t('textWed1')}</h3>
-                <p className=''><span className='font-bold'>{t('textWed2')}</span>{t('textWed3')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] p-1'><span className='font-bold'>{t('textWed2')}</span>{t('textWed3')}</p>
               </div>
-              <div>
+              <div className='flex flex-col gap-1'>
                 <h3 className="text-[#007ea7] font-bold">{t('text21')}</h3>
-                <p className='border-b-2 border-[#007ea7] pl-2 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text211')}</span>{t('text22')}</p>
-                <p className='border-b-2 border-[#007ea7] pl-2 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text222')}</span>{t('text23')}</p>
-                <p className='border-b-2 border-[#007ea7] pl-2 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text233')}</span>{t('text24')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] border-[#007ea7] p-1 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text211')}</span>{t('text22')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] border-[#007ea7] p-1 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text222')}</span>{t('text23')}</p>
+                <p className='border-2 sm:border-b-2 border-[#007ea7] border-[#007ea7] p-1 border-b-2 border-l-2 border-[#007ea7]'><span className='font-bold'>{t('text233')}</span>{t('text24')}</p>
               </div>
             </div>
             <p className="mt-4 text-gray-700">
