858845831420e0125ceed02c430f9b643d72ec44
Alberto <a.ramos@cyim.com>
2025-06-28 12:27:50 +0200

    upgraded galery

--- src/app/[local]/components/InstagramGallery.jsx
+++ src/app/[local]/components/InstagramGallery.jsx
@@ -4,26 +4,28 @@ import { useEffect } from 'react';
 
 export default function InstagramGallery() {
     useEffect(() => {
-        // Only add the script once
         const scriptId = 'lightwidget-script';
+
         if (!document.getElementById(scriptId)) {
             const script = document.createElement('script');
-            script.id = scriptId;
             script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
+            script.id = scriptId;
             script.async = true;
             document.body.appendChild(script);
         }
     }, []);
 
     return (
-        <div style={{ maxWidth: '100%', overflow: 'hidden' }}>
-            <iframe
-                src="//lightwidget.com/widgets/7c5f5f5978a959e6a00d60b3ede6ec51.html"
-                scrolling="no"
-                allowTransparency={true}
-                className="lightwidget-widget"
-                style={{ width: '100%', border: 0, overflow: 'hidden' }}
-            />
-        </div>
+        <iframe
+            src="https://cdn.lightwidget.com/widgets/7c5f5f5978a959e6a00d60b3ede6ec51.html"
+            scrolling="no"
+            allowTransparency="true"
+            className="lightwidget-widget"
+            style={{
+                width: '100%',
+                border: 0,
+                overflow: 'hidden',
+            }}
+        ></iframe>
     );
-}
+};
